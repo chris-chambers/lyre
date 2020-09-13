3336dfa0f47a8f45459aa31db7589c8da398b64a
Christopher Chambers <chris@peanutcode.com>
2020-09-13 12:45:55 -0400

    Remove `aeval` dep, improve module resolution, add random port selection
    - Python 3.8 exposes top-level `await` parsing, so it is not necessary to use
      the `aeval` package to do async evaluation.
    - Improves how modules are resolved from their filenames.
    - Changes the format of .lyre-port so that multiple IP/port pairs can be
      communicated to consumers.

--- lyre/__init__.py
+++ lyre/__init__.py
@@ -1,1 +1,5 @@
+import logging
+
 __version__ = '0.0.2'
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
--- lyre/server.py
+++ lyre/server.py
@@ -1,44 +1,68 @@
 import argparse
+import ast
+import functools
 import importlib
+from logging import getLogger
+import os
 from pathlib import Path
 from textwrap import dedent
 import traceback
 import sys
 import types
+from typing import Dict
 
-from aeval import aeval
 import lsp
 import trio
 
+from .evaluation import capture_last_expression, aexec
+from .resolution import ModuleResolveError, module_name_from_filename
 
-_ROOT_MARKER_FILES = [
-    'pyproject.toml',
-    'setup.py',
-    'requirements.txt',
-    'MANIFEST.in',
-    '.git',
-    '.hg',
-]
+
+_log = getLogger(__name__)
 
 
 def main():
+    import logging
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    _log.addHandler(ch)
+    _log.setLevel(logging.INFO)
+
     parser = argparse.ArgumentParser(allow_abbrev=False)
+    parser.add_argument('-H', '--host', default=None)
     parser.add_argument('-p', '--port', default=6768, type=int)
+    parser.add_argument('-f', '--portfile', default='./.lyre-port')
     args = parser.parse_args()
 
-    trio.run(run, args.port)
+    sys.path.insert(0, '.')
+
+    trio.run(functools.partial(
+        run,
+        host=args.host,
+        port=args.port,
+        portfile=args.portfile,
+    ))
 
 
-async def run(port=6768):
-    await trio.serve_tcp(_handler, port)
+async def run(host=None, port=6768, portfile='./.lyre-port'):
+    listeners = await trio.open_tcp_listeners(port, host=host)
+    if portfile:
+        with open(portfile, 'x') as f:
+            for listener in listeners:
+                host, port, *_ = listener.socket.getsockname()
+                f.write(f'{host} {port}\n')
+    try:
+        await trio.serve_listeners(_handler, listeners)
+    finally:
+        if portfile:
+            os.remove(portfile)
 
 
 async def _handler(stream: trio.SocketStream):
-    print('connected')
+    _log.info('connected')
+    conn = lsp.Connection('server')
+    recvsize = 4096
     try:
-        conn = lsp.Connection('server')
-        recvsize = 4096
-
         while True:
             while True:
                 event = conn.next_event()
@@ -57,85 +81,79 @@ async def _handler(stream: trio.SocketStream):
 
             _, body = conn.get_received_data()
             method = body.get('method', None)
+            _log.debug(f'handling: {method}')
             if method == 'initialize':
                 send_data = conn.send_json(dict(
                     id=body['id'],
                     result=dict(capabilities=dict()),
                 ))
                 await stream.send_all(send_data)
+            elif method == 'shutdown':
+                break
             elif method == 'lyre/eval':
-                import sys
-
-                p = body['params']['path']
-                modname = _module_name_from_filename(Path(p))
-
-                try:
-                    mod = importlib.import_module(modname)
-                except Exception:
-                    mod = sys.modules[modname] = types.ModuleType(
-                        modname, 'Synthetic module created by Lyre')
-                    mod.__file__ = p
-
-                # FIXME: Need to source map back to the indented version for
-                #        exceptions.
-                code = dedent(body['params']['code'])
-
-                response = dict(id=body['id'])
-                try:
-                    value = await aeval(code, mod.__dict__, None)
-                    response['result'] = dict(status='ok', value=repr(value))
-                except BaseException as ex:
-                    etype, ex, tb = sys.exc_info()
-                    fmt_exc = traceback.format_exception_only(etype, ex)
-                    fmt_tb = traceback.format_tb(tb)
-
-                    response['result'] = dict(
-                        status='error',
-                        error=fmt_exc[-1],
-                        fullError=fmt_exc,
-                        traceback=fmt_tb,
-                    )
-
+                response = await _eval(body)
                 send_data = conn.send_json(response)
                 await stream.send_all(send_data)
 
             conn.go_next_circle()
     finally:
-        print('disconnected')
+        _log.info('disconnected')
 
 
-def _module_name_from_filename(path: Path):
-    if path.name == '__init__.py':
-        path = path.parent
+async def _eval(request: Dict) -> Dict:
+    response = dict(id=request['id'])
 
-    for import_path in _candidate_import_paths(path):
-        try:
-            relpath = path.relative_to(import_path)
-        except ValueError:
-            continue
+    try:
+        params = request['params']
+        path = Path(params['path'])
+        code = params['code']
+        lineno = params.get('lineno', 1)
+    except KeyError:
+        # TODO: More precise error result.
+        response['result'] = _error_result()
+        return response
 
-        if not relpath.parts:
-            continue
+    try:
+        modname = module_name_from_filename(path)
+    except ModuleResolveError:
+        response['result'] = _error_result()
+        return response
 
-        parts = list(relpath.parts)
-        parts[-1] = relpath.stem
-        return '.'.join(parts)
+    try:
+        mod = importlib.import_module(modname)
+    except Exception:
+        print(f'import failed: {modname}')
+        mod = sys.modules[modname] = types.ModuleType(
+            modname, 'Synthetic module created by Lyre')
+        mod.__file__ = str(path)
 
-    # FIXME: Need a custom exception (or a better standard one).
-    raise Exception(f'cannot determine module name: {path}')
+    # TODO: Would be nice to source map back to the indented version for
+    #       SyntaxError exceptions, especially.
+    code = dedent(code)
+
+    try:
+        node = ast.parse(code, str(path), 'exec')
+        ast.increment_lineno(node, lineno - 1)
+        capture_last_expression(node, '--lyre-result--')
+
+        await aexec(node, str(path), mod.__dict__, mod.__dict__)
+        value = mod.__dict__.pop('--lyre-result--', None)
+        response['result'] = dict(status='ok', value=repr(value))
+    except BaseException:
+        response['result'] = _error_result()
+    finally:
+        mod.__dict__.pop('--lyre-result--', None)
+    return response
 
 
-def _candidate_import_paths(path: Path):
-    yield from (Path(path) for path in sys.path)
-    project_path = _guess_project_path(path)
-    if project_path is not None:
-        yield project_path
+def _error_result():
+    etype, ex, tb = sys.exc_info()
+    fmt_exc = traceback.format_exception_only(etype, ex)
+    fmt_tb = traceback.format_tb(tb)
 
-
-def _guess_project_path(path: Path):
-    for dir_ in path.parents:
-        for marker in _ROOT_MARKER_FILES:
-            if dir_.joinpath(marker).exists:
-                return dir_
-
-    return None
+    return dict(
+        status='error',
+        error=fmt_exc[-1],
+        fullError=fmt_exc,
+        traceback=fmt_tb,
+    )
